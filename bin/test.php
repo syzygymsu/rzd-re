f3eb2929f081c476c11c9d1acd67a4f7e06cc6b2
Anton A. Sumin <syzygymsu@gmail.com>
2016-03-08 22:57:03 +0300

    Поиск билетов туда и обратно

--- bin/test.php
+++ bin/test.php
@@ -4,8 +4,22 @@ require __DIR__ . '/../src/autoload.php';
 
 $api = new rzd\api\API();
 
-var_dump($api->ticketSearch()->searchTransfer(
+$tomorrow_dt = (new DateTime())->add(new DateInterval('P1D'));
+$tomorrow = rzd\api\Date::fromDateTime($tomorrow_dt);
+
+$a1 = $api->ticketSearch()->searchTransfer(
 		2000000, // Москва
 		2004000, // Санкт-Петербург
-		rzd\api\Date::fromDateTime(new DateTime())
-));
+		$tomorrow
+);
+
+
+list($a2, $b2) = $api->ticketSearch()->searchTransferAndReturn(
+		2000000, // Москва
+		2004000, // Санкт-Петербург
+		$tomorrow,
+		$tomorrow
+);
+
+// При поиске туда и обратно результат "туда" должен быть таким же, как и при поиске в одну сторону.
+var_dump($a1 == $a2);
--- src/rzd/api/TicketSearch.php
+++ src/rzd/api/TicketSearch.php
@@ -19,18 +19,18 @@ class TicketSearch {
 	/**
 	 * @param string $from Идентификатор станции отправления
 	 * @param string $to Идентификатор станции прибытия
-	 * @param \rzd\api\Date $from_date Дата отправления
+	 * @param \rzd\api\Date $departure Дата отправления
 	 * @param int $types Типы маршрутов, комбинация флагов TRAIN_*
 	 * @param bool $show_unavailable Также искать маршруты без доступных билетов
 	 * @return array
 	 */
-	public function searchTransfer($from, $to, Date $from_date, $types = self::TRAIN_ANY, $show_unavailable = false) {
+	public function searchTransfer($from, $to, Date $departure, $types = self::TRAIN_ANY, $show_unavailable = false) {
 		$args = [
 			'dir' => 0,
 			'tfl' => $types,
 			'checkSeats' => $show_unavailable ? 0 : 1,
 			'code0' => $from,
-			'dt0' => $this->transport->formatDate($from_date),
+			'dt0' => $this->transport->formatDate($departure),
 			'code1' => $to,
 		];
 
@@ -39,6 +39,34 @@ class TicketSearch {
 				[], $args
 		);
 
-		return $res['tp'];
+		return $res['tp'][0]['list'];
+	}
+
+	/**
+	 * @param string $from Идентификатор станции отправления
+	 * @param string $to Идентификатор станции прибытия
+	 * @param \rzd\api\Date $departure Дата отправления
+	 * @param \rzd\api\Date $return Дата возвращения
+	 * @param int $types Типы маршрутов, комбинация флагов TRAIN_*
+	 * @param bool $show_unavailable Также искать маршруты без доступных билетов
+	 * @return array
+	 */
+	public function searchTransferAndReturn($from, $to, Date $departure, Date $return, $types = self::TRAIN_ANY, $show_unavailable = false) {
+		$args = [
+			'dir' => 1,
+			'tfl' => $types,
+			'checkSeats' => $show_unavailable ? 0 : 1,
+			'code0' => $from,
+			'dt0' => $this->transport->formatDate($departure),
+			'code1' => $to,
+			'dt1' => $this->transport->formatDate($return),
+		];
+
+		$res = $this->transport->performRidCall(
+				'timetable/public/ru?STRUCTURE_ID=735&layer_id=5371',
+				[], $args
+		);
+
+		return array($res['tp'][0]['list'], $res['tp'][1]['list']);
 	}
 }
